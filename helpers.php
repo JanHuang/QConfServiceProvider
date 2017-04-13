7b2cfdd1c8aeb453d8754aef488166424fa90ab3
JanHuang <bboyjanhuang@gmail.com>
2017-04-13 21:15:08 +0800

    init

---
+++ helpers.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2016
+ *
+ * @see      https://www.github.com/janhuang
+ * @see      http://www.fast-d.cn/
+ */
+
+function qconf($name)
+{
+
+}
