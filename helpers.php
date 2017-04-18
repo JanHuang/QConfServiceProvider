56820be3c20c5d299c2f7e9c28cb4679f6408494
JanHuang <bboyjanhuang@gmail.com>
2017-04-18 10:31:47 +0800

    add qconf functions

--- helpers.php
+++ helpers.php
@@ -7,7 +7,24 @@
  * @see      http://www.fast-d.cn/
  */
 
-function qconf($name)
+/**
+ * @param $path
+ * @param null $idc
+ * @param null $flag
+ * @return string
+ */
+function qconf_get_value($path, $idc = null, $flag = null)
 {
+    return QConf::getConf($path, $idc, $flag);
+}
 
+/**
+ * @param $path
+ * @param null $idc
+ * @param null $flag
+ * @return array
+ */
+function qconf_get_values($path, $idc = null, $flag = null)
+{
+    return QConf::getBatchConf($path, $idc, $flag);
 }
