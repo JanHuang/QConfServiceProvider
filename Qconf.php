a2ec8b7b215a5ea728c1309cb3157d93c9729bad
Runnerlee <runnerleer@gmail.com>
2017-06-09 17:06:29 +0800

    fix class declare

--- Qconf.php
+++ Qconf.php
@@ -4,48 +4,48 @@
  * @email: runnerleer@gmail.com
  * @time: 2017-06
  */
-
-class Qconf
-{
-
-    /**
-     * @param $key
-     * @param $idc
-     * @param $get_flag
-     * @return string|null
-     */
-    public function getConf($key, $idc = null, $get_flag = null)
+if (!class_exists('Qconf')) {
+    class Qconf
     {
-    }
 
-    /**
-     * @param $key
-     * @param $idc
-     * @param $get_flag
-     * @return string|array|null
-     */
-    public function getBatchKeys($key, $idc = null, $get_flag = null)
-    {
-    }
+        /**
+         * @param $key
+         * @param $idc
+         * @param $get_flag
+         * @return string|null
+         */
+        public function getConf($key, $idc = null, $get_flag = null)
+        {
+        }
 
-    /**
-     * @param $path
-     * @param $idc
-     * @param $get_flag
-     * @return array|null
-     */
-    public function getAllHost($path, $idc = null, $get_flag = null)
-    {
-    }
+        /**
+         * @param $key
+         * @param $idc
+         * @param $get_flag
+         * @return string|array|null
+         */
+        public function getBatchKeys($key, $idc = null, $get_flag = null)
+        {
+        }
 
-    /**
-     * @param $path
-     * @param $idc
-     * @param $get_flag
-     * @return string|null
-     */
-    public function getHost($path, $idc = null, $get_flag = null)
-    {
-    }
+        /**
+         * @param $path
+         * @param $idc
+         * @param $get_flag
+         * @return array|null
+         */
+        public function getAllHost($path, $idc = null, $get_flag = null)
+        {
+        }
 
+        /**
+         * @param $path
+         * @param $idc
+         * @param $get_flag
+         * @return string|null
+         */
+        public function getHost($path, $idc = null, $get_flag = null)
+        {
+        }
+    }
 }
